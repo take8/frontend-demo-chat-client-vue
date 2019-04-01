f6fc43e2fb8e26ccb9015c1498ed361fca9d155f
take8 <37664118+take8@users.noreply.github.com>
2019-04-02 01:38:11 +0900

    メッセージを追加できるように修正

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,9 +1,18 @@
 export default {
   name: 'chat',
+  methods: {
+    sendMessage () {
+      this.messages.push(this.message)
+      this.message = ''
+    }
+  },
   // data: 現在のコンポーネントでのみ有効なデータの定義
   data () {
     return {
-      channels: ['general', 'random']
+      channels: ['general', 'random'],
+      message: '',
+      // フォームで入力したデータ
+      messages: []
     }
   }
 }
