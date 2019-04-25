c1e3dc84c7156777fbb0b3f913c5f8046e573681
take8 <37664118+take8@users.noreply.github.com>
2019-04-26 03:39:20 +0900

    プロパティの検証

--- src/components/MessageList/message_list.js
+++ src/components/MessageList/message_list.js
@@ -1,5 +1,12 @@
 export default {
   name: 'message-list',
   // "data"の代わりに"props"を定義するイメージ?
-  props: [ 'messages' ]
+  props: {
+    'messages': {
+      type: Array, // データの型(String, Number, Boolean, Function, Object, Array, Symbolのいずれか)
+      default: [], // デフォルト値
+      required: true // 必須か
+      // その他にも"validator"(検証するための関数を定義するイメージ)などの要素もある
+    }
+  }
 }
