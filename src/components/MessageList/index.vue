4bc0c806de93ac0ef05d894c2f4dc973d426f4f5
take8 <37664118+take8@users.noreply.github.com>
2019-04-28 22:10:23 +0900

    コンポーネントごとに異なるプリプロセッサを使用できる例

--- src/components/MessageList/index.vue
+++ src/components/MessageList/index.vue
@@ -1,2 +1,3 @@
-<template src="./message_list.pug" lang="pug"></template>
+<!-- <template src="./message_list.pug" lang="pug"></template> -->
+<template src="./message_list.html"></template>
 <script src="./message_list.js"></script>
